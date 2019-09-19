f2af8c956639480593f03ddd3ee18e412a79e934
jnmcshan <54905760+jnmcshan@users.noreply.github.com>
2019-09-19 16:23:21 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+var numberDiv = document.body.querySelector(".numero")= Total Billing;
+<head>
+<title>Your Total Bill</title>
+</head>
+
+<body>
+<hi>Billing</hi> 
+<title><Menu Items></title>;
+<script>    
+numberDiv.innerHTML ="Receipt";   
+var sum1="MenuItems";
+sum1=prompt("Receipt")
+var num1="MenuItems";
+var num2=(0.07)"tax";
+num2=prompt("tax")
+var sum2=(num1+num2)#"Total Receipt" + "Tax";
+var sum2=numberPrompt(MenuItems+tax_7%);
+numberDiv.innerHTML = "Bill w/tax";
+var sum2=(num1+num2);#total meal w/tax of 7%;
+var num3=(0.05)"gratuity";
+var sum3=(sum2+(sum2*num3))#"Total Receipt w/tax" + "Gratuity(5%)";
+var sumTotal=(sum1+sum1*num2)+(sum2*0.05);#"Meal plus gratuity of 5%""
+numberDiv.innerHTML = "Bill w/tax+gratuity"
+var sumTotal=numberPrompt(Your total is w/ tax and tip.)
+alert(sum3)
+</script>
+</body>
